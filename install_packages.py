cdcf8e9a62f613eaa57c414fa62e0de8bd2c80ed
DAVID-TECH-commits <davidadeola2022@gmail.com>
2025-08-13 15:09:01 +0100

    Delete install_packages.py
    Signed-off-by: DAVID-TECH-commits <davidadeola2022@gmail.com>

--- install_packages.py
+++
@@ -1,7 +0,0 @@
-import sys
-import subprocess
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "python-dotenv"])
-
